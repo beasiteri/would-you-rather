4b237d57f11e51a05b37d7999389553854492533
bea <siteribea@gmail.com>
2021-01-21 21:27:14 +0100

    Get users data from database

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <Provider>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </Provider>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
