b2ad487d8bde161d693d6c96ee709883e8599368
bea <siteribea@gmail.com>
2021-01-21 21:04:58 +0100

    Add browser router to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
-   <App />,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
