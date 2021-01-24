46f23f2a25e0c0e348248f1433df5191a3d40098
bea <siteribea@gmail.com>
2021-01-24 12:56:14 +0100

    Create mapStateToProps function

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Dashboatd extends Component {
     render() {
+        console.log(this.props)
         return (
             <div>
                 Dashboard
@@ -10,4 +12,11 @@ class Dashboatd extends Component {
     }
 }
 
-export default Dashboard;
+function mapStateToProps ({questions}) {
+    return {
+        questions: Object.keys(questions)
+        .sort((a,b) => question[b].timestamp - question[a].timestamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
