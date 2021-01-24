35543702e799ef098ad481739ba187e2137b4eaf
bea <siteribea@gmail.com>
2021-01-24 12:47:25 +0100

    Create dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboatd extends Component {
+    render() {
+        return (
+            <div>
+                Dashboard
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
