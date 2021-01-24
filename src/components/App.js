ece88ac4626207803a16f6c169dea39bb5a3d956
bea <siteribea@gmail.com>
2021-01-24 12:28:54 +0100

    Invoke the handleInitialData action creator in componentDidMount

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return(
       <div>Starter code</div>
