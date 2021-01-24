c5667f92da6b012e0f4b4e037eff8d965da5ea7c
bea <siteribea@gmail.com>
2021-01-24 12:57:09 +0100

    Import dashboard component to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard';
 
 class App extends Component {
   componentDidMount() {
@@ -8,7 +9,7 @@ class App extends Component {
   }
   render() {
     return(
-      <div>Starter code</div>
+      <div><Dashboard /></div>
     )
   }
 }
