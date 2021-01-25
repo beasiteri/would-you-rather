9a4160dbc6eadb5fbbe15d7b5fe1261656df6b85
bea <siteribea@gmail.com>
2021-01-25 20:26:14 +0100

    Add dashboard question navigation tabs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 import { connect } from 'react-redux';
 
 class Dashboard extends Component {
+    state = {
+        activeTab: '1'
+    };
+
+    toggleTab = tab => {
+        if (this.state.activeTab !== tab) {
+            this.setState({ activeTab: tab });
+        } 
+    };
+
     render() {
         console.log(this.props)
         return (
             <div>
-                Dashboard
+                <Nav>
+                    <NavItem>
+                        <NavLink onClick={() => {this.toggleTab('1')}} 
+                            className={{active: this.state.activeTab === '1'}}>
+                            Unanswered
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink onClick={() => {this.toggleTab('2')}}
+                            className={{active: this.state.activeTab === '2'}}>
+                            Answered
+                        </NavLink>
+                    </NavItem>
+                    
+                </Nav>
             </div>
         )
     }
