7e479200ed4f41b04cc685b247a96d85006515f5
bea <siteribea@gmail.com>
2021-01-26 00:31:43 +0100

    Add navbar navigation components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
 
 class Navbar extends Component {
     render() {
         return(
             <div>
-                Navbar
+                <Navbar>
+                    <Fragment>
+                        <NavbarToggler>
+                            <Collapse>
+                                <Nav>
+                                    <NavItem>
+                                        <NavLink>Home</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink>New Question</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink>Leader Board</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink>UserName</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink>Logout</NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </Collapse>
+                        </NavbarToggler>
+                    </Fragment>
+                </Navbar>
             </div>
         )
     }
