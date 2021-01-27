b1cb1b2e749cea37d2465da5036a82b6b0e94eb6
bea <siteribea@gmail.com>
2021-01-27 20:07:14 +0100

    Import proptypes

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,35 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './Routes';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
+
+  mapDispatchToProps(dispatch) {
+    return {
+      handleInitialData: () => {
+        dispatch(handleInitialData())
+      }
+    }
+  }
+
   render() {
+    const { notLoggedIn } = this.props;
+
+    App.propTypes = {
+      handleInitialData : PropTypes.func.isRequired,
+      notLoggedIn: PropTypes.bool.isRequired
+    };
+
     return(
       <Router>
         <Fragment>
           <div className="main-container">
-            test
+            <Routes notLoggedIn={notLoggedIn}/>
           </div>
         </Fragment>
       </Router>
