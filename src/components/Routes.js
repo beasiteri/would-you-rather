15d81dce722cecfd5ebf3045f3eb8461f4b5148f
bea <siteribea@gmail.com>
2021-01-27 19:54:06 +0100

    Create routes component

---
+++ src/components/Routes.js
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+
+class Routes extends Component {
+    render() {
+        const { notLoggedIn } = this.props;
+
+        Routes.propTypes = {notLoggedIn: PropTypes.any};
+
+        return(
+            <Switch>
+                {
+                    notLoggedIn ? 
+                    <Route path="/" exact component={Login}/> :
+                    <Fragment>
+                        <Route path='/' exact component={Dashboard} />
+                    </Fragment>
+                }
+            </Switch>
+        )
+    }
+}
+
+export default Routes;
