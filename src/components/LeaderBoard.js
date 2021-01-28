df4ccceb486d2ada91b6eee78804bd619a06e092
bea <siteribea@gmail.com>
2021-01-28 17:53:16 +0100

    Create leaderboard component

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LeaderBoard extends Component {
+    render() {
+        return(
+            <div>
+                test LeaderBoard
+            </div>
+        )
+    }
+}
+
+export default LeaderBoard;
