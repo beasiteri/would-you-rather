cca489199772006c418e267a4471dfc44f619011
bea <siteribea@gmail.com>
2021-01-28 17:24:10 +0100

    Add style to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink, TabContent,TabPane, Row, Col} from 'reactstrap';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
+import Question from './Question';
+import PropTypes from 'prop-types';
 
 class Dashboard extends Component {
     state = {
@@ -15,9 +17,16 @@ class Dashboard extends Component {
     };
 
     render() {
+        const { unansweredQuestions, answeredQuestions } = this.props;
+
+        Dashboard.propTypes = {
+            answeredPolls : PropTypes.array,
+            unansweredPolls : PropTypes.array
+        };
+
         return (
             <div className="dashboard">
-                <Nav className="d-flex flex-nowrap">
+                <Nav className="d-flex flex-nowrap">    
                     <NavItem className="navItem w-100 text-center">
                         <NavLink onClick={() => {this.toggleTab('1')}}
                             className={classnames({ active: this.state.activeTab === '1' })}>
@@ -30,17 +39,41 @@ class Dashboard extends Component {
                             Answered
                         </NavLink>
                     </NavItem>
-                    
                 </Nav>
+
+                <TabContent className="tab-content" activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <Row>
+                        {unansweredQuestions.map(qid =>
+                            <Col key={qid}>
+                                <Question id={qid}/>
+                            </Col>
+                        )}
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <Row>
+                        {answeredQuestions.map(qid =>
+                            <Col key={qid}>
+                                <Question id={qid}/>
+                            </Col>
+                        )}
+                        </Row>
+                    </TabPane>
+                </TabContent>
             </div>
         )
     }
 }
 
-function mapStateToProps ({questions}) {
+function mapStateToProps ({ questions, users, authedUser }) {
+    const user = users[authedUser];
+    const answeredQuestions = Object.keys(user.answers)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp);
     return {
-        questions: Object.keys(questions)
-        .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+      unansweredQuestions : Object.keys(questions).filter(qid => !answeredQuestions.includes(qid))
+        .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
+      answeredQuestions
     }
 }
 
