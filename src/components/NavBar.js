449764f49b85b6b79a7f3481b11335cfe7fbcf05
bea <siteribea@gmail.com>
2021-01-28 11:28:45 +0100

    Refactor navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,55 @@
 import React, { Component, Fragment } from 'react';
 import { Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import {connect} from "react-redux";
 
 class NavBar extends Component {
+    state = {
+        isOpen: false
+    };
+
+    toggleNavbar = () =>  {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+    };
+
     render() {
         const { authedUser } = this.props;
-        
+
         NavBar.propTypes = {
             authedUser: PropTypes.string,
         };
 
         return(
             <div>
-                <Navbar>
+                <Navbar bg="primary" variant="dark" light expand="md">
                 {authedUser && 
-                    <Fragment>
-                        <NavbarToggler>
-                            <Collapse>
-                                <Nav>
-                                    <NavItem>
-                                        <NavLink>Home</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink>New Question</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink>Leader Board</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink>UserName</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink>Logout</NavLink>
-                                    </NavItem>
-                                </Nav>
-                            </Collapse>
-                        </NavbarToggler>
-                    </Fragment>
-                
+                <Fragment>
+                    <NavbarToggler onClick={this.toggleNavbar} />
+                    <Collapse isOpen={this.state.isOpen} navbar className="d-flex justify-content-between">
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink>Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>New Question</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>Leader Board</NavLink>
+                            </NavItem>
+                        </Nav>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink>UserName</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>Logout</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Fragment>
                 }
                 </Navbar>
             </div>
@@ -45,4 +57,10 @@ class NavBar extends Component {
     }
 }
 
-export default NavBar;
+function mapStateToProps ({ authedUser }) {
+    return {
+      authedUser
+    }
+}
+
+export default withRouter(connect(mapStateToProps, null)(NavBar));
