4db8d9a394c2db96c6ae304b850fd83548732653
bea <siteribea@gmail.com>
2021-01-28 19:20:03 +0100

    Style navbar elements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,27 @@ import { Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstr
 import PropTypes from "prop-types";
 import { withRouter, Link } from "react-router-dom";
 import {connect} from "react-redux";
+import classnames from 'classnames';
 
 class NavBar extends Component {
     state = {
-        isOpen: false
+        isOpen: false,
+        activeTab: 'home'
     };
 
     toggleNavbar = () =>  {
         this.setState({
-          isOpen: !this.state.isOpen
+            isOpen: !this.state.isOpen
         });
     };
+
+    toggleTab(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
 
     render() {
         const { authedUser } = this.props;
@@ -24,20 +34,32 @@ class NavBar extends Component {
 
         return(
             <div>
-                <Navbar bg="primary" variant="dark" light expand="md">
+                <Navbar expand="md">
                 {authedUser && 
                 <Fragment>
                     <NavbarToggler onClick={this.toggleNavbar} />
                     <Collapse isOpen={this.state.isOpen} navbar className="d-flex justify-content-between">
                         <Nav navbar>
                             <NavItem>
-                                <NavLink tag={Link} to="/">Home</NavLink>
+                                <NavLink 
+                                    className={classnames({ active: this.state.activeTab === 'home' })}
+                                    onClick={() => { this.toggleTab('home'); }} 
+                                    tag={Link} 
+                                    to="/">Home</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink tag={Link} to="/add">New Question</NavLink>
+                                <NavLink 
+                                    className={classnames({ active: this.state.activeTab === 'newquestion' })}
+                                    onClick={() => { this.toggleTab('newquestion'); }} 
+                                    tag={Link} 
+                                    to="/add">New Question</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink tag={Link} to="/leaderboard">Leader Board</NavLink>
+                                <NavLink 
+                                    className={classnames({ active: this.state.activeTab === 'leaderboard' })}
+                                    onClick={() => { this.toggleTab('leaderboard'); }}
+                                    tag={Link} 
+                                    to="/leaderboard">Leader Board</NavLink>
                             </NavItem>
                         </Nav>
                         <Nav navbar>
@@ -45,7 +67,11 @@ class NavBar extends Component {
                                 <NavLink>UserName</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink tag={Link} to="/logout">Logout</NavLink>
+                                <NavLink 
+                                    className={classnames({ active: this.state.activeTab === 'logout' })}
+                                    onClick={() => { this.toggleTab('logout'); }}
+                                    tag={Link} 
+                                    to="/logout">Logout</NavLink>
                             </NavItem>
                         </Nav>
                     </Collapse>
