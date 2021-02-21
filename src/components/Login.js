5bf1b3233210a5df5de1802f39cc7af5b4a5cfe9
bea <siteribea@gmail.com>
2021-02-21 11:15:02 +0100

    Add handle change user function to login container

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,56 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
+import { Row, Col, Form, FormGroup, Label, Input } from "reactstrap";
+import PropTypes from 'prop-types';
+import { setAuthedUser } from '../actions/authedUser';
+import { connect } from 'react-redux';
 
 class Login extends Component {
-    render() {
-        return(
-            <div>
-                test
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {userId : ''};
+    this.handleChangeUser = this.handleChangeUser.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeUser(event) {
+    this.setState({userId: event.target.value});
+  }
+
+  render() {
+      const { userId } = this.state; 
+
+      Login.propTypes = {
+          users: PropTypes.object.isRequired,
+          authenticate: PropTypes.func.isRequired
+      };
+
+      return(
+          <Row>
+              <Col md="12">
+                  <Form>
+                      <FormGroup>
+                          <Label for="userSelect">Sign In</Label>
+                          <Input type="select" id="userSelect" name="select" value={userId} />
+                      </FormGroup>
+                  </Form>
+              </Col>
+          </Row>
+      )
+  }
 }
 
-export default Login;
+function mapStateToProps ({ users }) {
+    return {
+      users
+    }
+  }
+
+function mapDispatchToProps(dispatch) {
+    return {
+      authenticate: (id) => {
+        dispatch(setAuthedUser(id))
+      }
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
