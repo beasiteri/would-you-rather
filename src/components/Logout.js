cd0afa0db94e60850e352e1d64531348d6da17a4
bea <siteribea@gmail.com>
2021-02-21 11:29:19 +0100

    Add componentWillMount to unset authed user

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { unsetAuthedUser } from '../actions/authedUser';
 
 
 class Logout extends Component {
+    componentWillMount () {
+        this.props.dispatch(unsetAuthedUser())
+    }
+
     render() {
         return <Redirect to='/' />
     }
