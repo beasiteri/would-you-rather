bb188554b59675577b791f268795ef66ac83819c
bea <siteribea@gmail.com>
2021-02-21 11:23:37 +0100

    Add submit function to login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,8 +16,20 @@ class Login extends Component {
     this.setState({userId: event.target.value});
   }
 
+  handleSubmit(event) {
+    const { userId } = this.state;
+    const { authenticate } = this.props;
+    if (userId) {
+      authenticate(userId);
+    } else {
+      alert('Please select a user');
+    }
+    event.preventDefault();
+  }
+
   render() {
       const { userId } = this.state; 
+      const { users } = this.props;
 
       Login.propTypes = {
           users: PropTypes.object.isRequired,
@@ -27,11 +39,14 @@ class Login extends Component {
       return(
           <Row>
               <Col md="12">
-                  <Form>
+                  <Form onSubmit={this.handleSubmit}>
                       <FormGroup>
                           <Label for="userSelect">Sign In</Label>
-                          <Input type="select" id="userSelect" name="select" value={userId} />
+                          <Input type="select" id="userSelect" name="select" value={userId}
+                          onChange={this.handleChangeUser} />
+                          <option value="" disabled>Select User</option>
                       </FormGroup>
+                      <input disabled={userId === ''} type="submit" value="Sign In" />
                   </Form>
               </Col>
           </Row>
